25e24bdb574c9ed6247b658d757df00d6e95340e
Shakthi-2406 <shakthisri504@gmail.com>
2024-07-26 11:01:35 +0530

    added bibernate

--- dropwizard-rest-app/src/main/java/com/dropwizard/model/Pokemon.java
+++ dropwizard-rest-app/src/main/java/com/dropwizard/model/Pokemon.java
@@ -1,4 +1,12 @@
 package com.dropwizard.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pokemon")
 public class Pokemon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 }
